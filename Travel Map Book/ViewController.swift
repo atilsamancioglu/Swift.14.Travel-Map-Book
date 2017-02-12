e0829b375071ce85dcc6917a2f1d53d8a1d037e0
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:21:38 +0300

    final version

--- Travel Map Book/ViewController.swift
+++ Travel Map Book/ViewController.swift
@@ -7,19 +7,170 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
+    var chosenLatitude : Double = 0
+    var chosenLongitude : Double = 0
+    
+    var transmittedTitle = ""
+    var transmittedSubtitle = ""
+    var transmittedLatitude : Double = 0
+    var transmittedLongitude : Double = 0
+    
+    var requestCLLocation = CLLocation()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        //mapview and location manager attributes
+        
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        // create gesture recognition
+        
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.chooseLocation(gestureRecognizer:)))
+        recognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(recognizer)
+        
+        
+        if transmittedTitle != "" {
+            
+            let annotation = MKPointAnnotation()
+            let coordinate = CLLocationCoordinate2D(latitude: self.transmittedLatitude, longitude: self.transmittedLongitude)
+            annotation.coordinate = coordinate
+            annotation.title = self.transmittedTitle
+            annotation.subtitle = self.transmittedSubtitle
+            self.mapView.addAnnotation(annotation)
+            
+            self.nameText.text = self.transmittedTitle
+            self.commentText.text = self.transmittedSubtitle
+            
+            
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.mapView.setRegion(region, animated: true)
+        
     }
+    
 
+    func chooseLocation(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let chosenCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            self.chosenLatitude = chosenCoordinates.latitude
+            self.chosenLongitude = chosenCoordinates.longitude
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = chosenCoordinates
+            annotation.title = nameText.text
+            annotation.subtitle = commentText.text
+            self.mapView.addAnnotation(annotation)
+            
+        }
+        
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "myAnnotation"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            pinView?.pinTintColor = UIColor.red
+            
+            let button = UIButton(type: .detailDisclosure)
+            pinView?.rightCalloutAccessoryView = button
+        } else {
+            pinView?.annotation = annotation
+        }
+        
+        return pinView
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if transmittedLatitude != 0 {
+            if transmittedLongitude != 0 {
+                self.requestCLLocation = CLLocation(latitude: transmittedLatitude, longitude: transmittedLongitude)
+            }
+        }
+        
+        CLGeocoder().reverseGeocodeLocation(requestCLLocation) { (placemarks, error) in
+            
+            if let placemark = placemarks {
+                
+                if placemark.count > 0 {
+                    
+                    let newPlaceMark = MKPlacemark(placemark: placemark[0])
+                    let item = MKMapItem(placemark: newPlaceMark)
+                    item.name = self.transmittedTitle
+                    
+                    let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+                    item.openInMaps(launchOptions: launchOptions)
+                    
+                }
+            }
+        }
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newLocation = NSEntityDescription.insertNewObject(forEntityName: "Locations", into: context)
+        
+        newLocation.setValue(nameText.text, forKey: "title")
+        newLocation.setValue(commentText.text, forKey: "subtitle")
+        newLocation.setValue(self.chosenLatitude, forKey: "latitude")
+        newLocation.setValue(self.chosenLongitude, forKey: "longitude")
+        
+        
+        do {
+            try context.save()
+            print("we managed to save it")
+        } catch {
+            print("error")
+        }
+     
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newLocationCreated"), object: nil)
+        _ = self.navigationController?.popViewController(animated: true)
+        
+    }
 
 }
 
